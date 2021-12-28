0d833f9d5c1f22a9b6dbbac9c53f5679534b3e71
Hoàng Mạnh Khiêm <khiemhm04@gmail.com>
2021-12-28 15:03:13 +0700

    x-ui-main

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# x-ui-main
- 
+# x-ui
+Hỗ trợ bảng điều khiển xray đa giao thức và nhiều người dùng
+
+# Đặc trưng
+- Giám sát tình trạng hệ thống
+-Hỗ trợ đa người dùng và đa giao thức, hoạt động trực quan hóa trang web
+-Giao thức được hỗ trợ: vmess, vless, trojan, shadowsocks, dokodemo-door, tất, http
+-Hỗ trợ cấu hình nhiều cấu hình truyền hơn
+- Thống kê lưu lượng, giới hạn lưu lượng, giới hạn thời gian hết hạn
+-Mẫu cấu hình xray có thể tùy chỉnh
+-Hỗ trợ bảng điều khiển truy cập https (mang theo tên miền + chứng chỉ ssl của riêng bạn)
+-Các mục cấu hình nâng cao hơn, xem bảng điều khiển để biết chi tiết
+
+# Cài đặt & Nâng cấp
+```
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+```
+
+## Cài đặt và nâng cấp thủ công
+1. Đầu tiên tải xuống gói nén mới nhất từ ​​https://github.com/vaxilu/x-ui/releases, thường chọn kiến ​​trúc `amd64`
+2. Sau đó tải gói nén lên thư mục `/ root /` của máy chủ và sử dụng người dùng `root` để đăng nhập vào máy chủ
+
+> Nếu kiến ​​trúc cpu máy chủ của bạn không phải là `amd64`, hãy thay thế` amd64` trong lệnh bằng một kiến ​​trúc khác
+
+```
+cd /root/
+rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+tar zxvf x-ui-linux-amd64.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+cp -f x-ui/x-ui.service /etc/systemd/system/
+mv x-ui/ /usr/local/
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
+```
+
+## Cài đặt bằng docker
+
+> Hướng dẫn về docker và hình ảnh về docker này được tạo bởi[Chasing66](https://github.com/Chasing66)提供
+
+1. Cài đặt docker
+```shell
+curl -fsSL https://get.docker.com | sh
+```
+2. Cài đặt x-ui
+```shell
+mkdir x-ui && cd x-ui
+docker run -itd --network=host \
+    -v $PWD/db/:/etc/x-ui/ \
+    -v $PWD/cert/:/root/cert/ \
+    --name x-ui --restart=unless-stopped \
+    enwaiax/x-ui:latest
+```
+> Xây dựng hình ảnh của riêng bạn
+```shell
+docker build -t x-ui .
+```
+## Hệ thống đề xuất
+-CentOS 7+
+-Ubuntu 16+
+-Debian 8+
+
+# vấn đề thường gặp
+
+## Di chuyển từ v2-ui
+Trước tiên hãy cài đặt phiên bản mới nhất của x-ui trên máy chủ nơi v2-ui được cài đặt, sau đó sử dụng lệnh sau để di chuyển, lệnh này sẽ di chuyển `tất cả dữ liệu tài khoản gửi đến 'của v2-ui của máy sang x-ui,' cài đặt bảng điều khiển và tên người dùng và mật khẩu Sẽ không di chuyển`
+> Sau khi di chuyển thành công, vui lòng `đóng v2-ui` và` khởi động lại x-ui`, nếu không quá trình chuyển đến của v2-ui và đầu vào của x-ui sẽ gây ra `xung đột cổng '
+``
+x-ui v2-ui
+``
+
+## vấn đề đóng lại
+Tất cả các loại vấn đề của Xiaobai đều thấy huyết áp cao
+
+## Stargazers theo thời gian
+
+[! [thời gian](https://starchart.cc/vaxilu/x-ui.svg)](https://starchart.cc/vaxilu/x-ui)
---
+++ web/service/inbound.go
@@ -0,0 +1,177 @@
+package service
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"time"
+	"x-ui/database"
+	"x-ui/database/model"
+	"x-ui/util/common"
+	"x-ui/xray"
+)
+
+type InboundService struct {
+}
+
+func (s *InboundService) GetInbounds(userId int) ([]*model.Inbound, error) {
+	db := database.GetDB()
+	var inbounds []*model.Inbound
+	err := db.Model(model.Inbound{}).Where("user_id = ?", userId).Find(&inbounds).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return inbounds, nil
+}
+
+func (s *InboundService) GetAllInbounds() ([]*model.Inbound, error) {
+	db := database.GetDB()
+	var inbounds []*model.Inbound
+	err := db.Model(model.Inbound{}).Find(&inbounds).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return inbounds, nil
+}
+
+func (s *InboundService) checkPortExist(port int, ignoreId int) (bool, error) {
+	db := database.GetDB()
+	db = db.Model(model.Inbound{}).Where("port = ?", port)
+	if ignoreId > 0 {
+		db = db.Where("id != ?", ignoreId)
+	}
+	var count int64
+	err := db.Count(&count).Error
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
+
+func (s *InboundService) AddInbound(inbound *model.Inbound) error {
+	exist, err := s.checkPortExist(inbound.Port, 0)
+	if err != nil {
+		return err
+	}
+	if exist {
+		return common.NewError("端口已存在:", inbound.Port)
+	}
+	db := database.GetDB()
+	return db.Save(inbound).Error
+}
+
+func (s *InboundService) AddInbounds(inbounds []*model.Inbound) error {
+	for _, inbound := range inbounds {
+		exist, err := s.checkPortExist(inbound.Port, 0)
+		if err != nil {
+			return err
+		}
+		if exist {
+			return common.NewError("端口已存在:", inbound.Port)
+		}
+	}
+
+	db := database.GetDB()
+	tx := db.Begin()
+	var err error
+	defer func() {
+		if err == nil {
+			tx.Commit()
+		} else {
+			tx.Rollback()
+		}
+	}()
+
+	for _, inbound := range inbounds {
+		err = tx.Save(inbound).Error
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *InboundService) DelInbound(id int) error {
+	db := database.GetDB()
+	return db.Delete(model.Inbound{}, id).Error
+}
+
+func (s *InboundService) GetInbound(id int) (*model.Inbound, error) {
+	db := database.GetDB()
+	inbound := &model.Inbound{}
+	err := db.Model(model.Inbound{}).First(inbound, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return inbound, nil
+}
+
+func (s *InboundService) UpdateInbound(inbound *model.Inbound) error {
+	exist, err := s.checkPortExist(inbound.Port, inbound.Id)
+	if err != nil {
+		return err
+	}
+	if exist {
+		return common.NewError("端口已存在:", inbound.Port)
+	}
+
+	oldInbound, err := s.GetInbound(inbound.Id)
+	if err != nil {
+		return err
+	}
+	oldInbound.Up = inbound.Up
+	oldInbound.Down = inbound.Down
+	oldInbound.Total = inbound.Total
+	oldInbound.Remark = inbound.Remark
+	oldInbound.Enable = inbound.Enable
+	oldInbound.ExpiryTime = inbound.ExpiryTime
+	oldInbound.Listen = inbound.Listen
+	oldInbound.Port = inbound.Port
+	oldInbound.Protocol = inbound.Protocol
+	oldInbound.Settings = inbound.Settings
+	oldInbound.StreamSettings = inbound.StreamSettings
+	oldInbound.Sniffing = inbound.Sniffing
+	oldInbound.Tag = fmt.Sprintf("inbound-%v", inbound.Port)
+
+	db := database.GetDB()
+	return db.Save(oldInbound).Error
+}
+
+func (s *InboundService) AddTraffic(traffics []*xray.Traffic) (err error) {
+	if len(traffics) == 0 {
+		return nil
+	}
+	db := database.GetDB()
+	db = db.Model(model.Inbound{})
+	tx := db.Begin()
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+	for _, traffic := range traffics {
+		if traffic.IsInbound {
+			err = tx.Where("tag = ?", traffic.Tag).
+				UpdateColumn("up", gorm.Expr("up + ?", traffic.Up)).
+				UpdateColumn("down", gorm.Expr("down + ?", traffic.Down)).
+				Error
+			if err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func (s *InboundService) DisableInvalidInbounds() (int64, error) {
+	db := database.GetDB()
+	now := time.Now().Unix() * 1000
+	result := db.Model(model.Inbound{}).
+		Where("((total > 0 and up + down >= total) or (expiry_time > 0 and expiry_time <= ?)) and enable = ?", now, true).
+		Update("enable", false)
+	err := result.Error
+	count := result.RowsAffected
+	return count, err
+}
